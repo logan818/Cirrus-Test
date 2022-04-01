5b5f972563e010bcbf7db35625750519e9e5834a
logan818 <logan.calhoun45@gmail.com>
2022-03-31 20:56:36 -0400

    updated npm react-script

--- cirrus-test/src/App.js
+++ cirrus-test/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import 'cirrus-ui'; 
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header>Logan Calhoun</header>
+      <Header />
       <button>Test</button>
     </div>
   );
---
+++ cirrus-test/src/Components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import 'cirrus-ui';
+
+function Header() {
+    return (
+        <header>Logan Calhoun</header>
+    );
+}
+
+export default Header;
